28dc42c20a8e4e8d9727062127579e7ba7f4a2ea
sowmya172 <sowmyasudhagani5.com>
2025-02-06 09:49:06 +0530

    my first commit

---
+++ demo062.java
@@ -0,0 +1,12 @@
+public class demo062 {
+    public static void main(String[] args) {
+        int[] array = {10, 20, 30};
+        if (array.length >= 2) {
+            System.out.println("1st element: " + array[0]);
+            System.out.println("2nd element: " + array[1]);
+            System.out.println("Last element: " + array[array.length - 1]);
+        } else {
+            System.out.println("Array does not have enough elements.");
+        }
+    }
+}
